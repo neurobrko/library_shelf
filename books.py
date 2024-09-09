f2e494fb8ca5081c87b2d9f67af7d7579e5eea24
neurobrko <elvis@elvis.sk>
2024-09-09 09:30:36 +0200

    04 - OpenAPI implementation

--- books.py
+++ books.py
@@ -1,6 +1,7 @@
 from flask import abort, make_response
 from config import db
 from models import Book, book_schema, books_schema
+from datetime import datetime
 
 
 def list_all():
@@ -57,6 +58,20 @@ def list_by_author(author):
         abort(404, f"There is no book on the shelf by {author}.")
 
 
+def update_book(isbn, book):
+    existing_book = Book.query.filter(Book.isbn == isbn).one_or_none()
+
+    if existing_book is not None:
+        if "pub_date" in book.keys():
+            book["pub_date"] = datetime.strptime(book["pub_date"], "%Y-%m-%d").date()
+        db.session.query(Book).filter(Book.isbn == isbn).update(
+            book, synchronize_session=False
+        )
+        db.session.commit()
+    else:
+        abort(404, f"There is no book with IDBN {isbn}.")
+
+
 def delete_book(id):
     existing_book = Book.query.filter(Book.id == id).one_or_none()
 
@@ -65,7 +80,7 @@ def delete_book(id):
         db.session.delete(existing_book)
         db.session.commit()
         return make_response(
-            f"Book with titile {existing_title} successfully deleted.", 200
+            f"Book with title {existing_title} successfully deleted.", 200
         )
     else:
         abort(404, f"There is no book with ID {id}.")
